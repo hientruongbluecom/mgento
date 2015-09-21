9dc28e18fd7d6ead6eb1dbeb2ac3b59945fe4949
Hien Truong <hien.truong@bluecomgroup.com>
2015-09-21 16:04:52 +0700

    2. Create A Custom Order State

--- app/code/local/Bluecom/CustomAttr/Model/Observer.php
+++ app/code/local/Bluecom/CustomAttr/Model/Observer.php
@@ -1,7 +1,7 @@
 <?php
 class Bluecom_CustomAttr_Model_Observer extends Varien_Object
 {
-    public function salesQuoteItemSetCustomAttribute($observer){
+    public function setCustomAttribute(Varien_Event_Observer $observer){
         $item = $observer->getQuoteItem();
         $product = $observer->getProduct();
         $item->setCustomAttribute($product->getCustomAttribute());
